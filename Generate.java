c50cc54a8ad2c79bc3472f50a659223382cbdf5e
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-07-27 03:30:01 -0400

    Revised Version
    Revised Version, want to add special characters.

--- Generate.java
+++ Generate.java
@@ -1,15 +1,18 @@
 import java.util.Random;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Generate
 {
+    private int numberOfCharacters, numOfSpecialCharacters, numberOfCapitalLetters, numberOfLowerCaseLetters;
     private Random rand;
-    private boolean specialCharacters, numbers;
-    private int numOfCapitals;
-    private char[] alphabet = {
+    private boolean specialCharacters, hasNumbers;
+    private String password = "";
+    private char[] uppercaseAlphabet = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     };
-    private char[] lowerCaseAlphabet = {
+    private char[] lowercaseAlphabet = {
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     };
@@ -17,20 +20,59 @@ public class Generate
         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=',
         '{', '}', '[', ']', '|', '\\', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/'
     };
+    private char[] numbersArray = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    };
 
-    public Generate(int numberOfCapitalLetters, boolean hasSpecialCharacters, boolean hasNumbers)
+    public Generate(int numberOfCapitalLetters, boolean hasSpecialCharacters, boolean hasNumbers, int numberOfCharacters)
     {
-        this.numOfCapitals = numberOfCapitalLetters;
-        this.specialCharacters = hasSpecialCharacters;
-        this.numbers = hasNumbers;
+        this.numberOfCharacters = numberOfCharacters;
+        this.numOfSpecialCharacters = hasSpecialCharacters ? numberOfCharacters / 3 : 0;
+        this.numberOfCapitalLetters = numberOfCapitalLetters;
+        this.numberOfLowerCaseLetters = numberOfCharacters - numberOfCapitalLetters - numOfSpecialCharacters;
+        if (hasNumbers)
+        {
+            this.numberOfLowerCaseLetters -= numberOfCharacters / 6;
+        }
+        rand = new Random();
     }
 
     public String generatePassword()
     {
-        for(char ch : specialSymbolsArray)
+        ArrayList<Character> passwordChars = new ArrayList<>();
+        
+        for (int i = 0; i < numberOfCapitalLetters; i++)
         {
-            System.out.println(ch);
+            passwordChars.add(uppercaseAlphabet[rand.nextInt(26)]);
         }
-        return "";
+        
+        for (int i = 0; i < numberOfLowerCaseLetters; i++)
+        {
+            passwordChars.add(lowercaseAlphabet[rand.nextInt(26)]);
+        }
+        
+        if (specialCharacters)
+        {
+            for (int i = 0; i < numOfSpecialCharacters; i++)
+            {
+                passwordChars.add(specialSymbolsArray[rand.nextInt(30)]);
+            }
+        }
+        
+        if (hasNumbers)
+        {
+            for (int i = 0; i < numberOfCharacters / 6; i++)
+            {
+                passwordChars.add(numbersArray[rand.nextInt(10)]);
+            }
+        }
+        
+        Collections.shuffle(passwordChars);
+    
+        for (char c : passwordChars)
+        {
+            password += c;
+        }
+        return password;
     }
 }
--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,9 +1,10 @@
+
 public class PasswordGenerator
 {
     public static void main(String[] args)
     {
-        Generate gen = new Generate(0, false, false);
-        String i = gen.generatePassword();
-        System.out.println(i);
+        Generate gen = new Generate(10, true, true, 30);
+        String password = gen.generatePassword();
+        System.out.println(password);
     }
 }
